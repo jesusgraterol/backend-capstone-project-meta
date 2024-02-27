d05778978cfab811858c90a3f7b34fde93ae3a8b
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-02-27 10:55:07 -0400

    Initialize the Booking and Menu Models
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db.models import Model, CharField, SmallIntegerField, DateTimeField, DecimalField
 
 # Create your models here.
+
+class Booking(Model):
+  name = CharField(max_length = 255)
+  no_of_guests = SmallIntegerField()
+  booking_date = DateTimeField()
+
+
+class Menu(Model):
+  title = CharField(max_length = 255)
+  price = DecimalField(max_digits = 10, decimal_places = 2)
+  inventory = SmallIntegerField()
